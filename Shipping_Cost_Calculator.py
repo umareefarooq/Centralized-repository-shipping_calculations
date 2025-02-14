35bcc55803910ce7a0ac781f9971faed994d7e56
umareefarooq <umareefarooq@gmail.com>
2025-02-14 08:35:43 +0100

    added additional line to Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -11,3 +11,5 @@ shipping_cost = weight * rate
 print(f"Shipping Cost: {shipping_cost} USD")
 
  # Here is a new update by <umareefarooq> via Macbook terminal 
+
+ # Here is another update by <umareefarooq> via Macbook terminal which will be reverted
